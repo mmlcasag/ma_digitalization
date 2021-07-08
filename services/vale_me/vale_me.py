d76b05bf0040b148712dcf55d6dfb37dae54537e
Francis Gregori <francis.g.munis@gmail.com>
2021-07-08 12:07:16 -0300

    refactor: update several rules in vale_me script

--- services/vale_me/vale_me.py
+++ services/vale_me/vale_me.py
@@ -11,7 +11,17 @@ logger = Logger.__call__().get_logger()
 
 
 def handle_sheet_row(row, index=0, prefix="", empty_value="", as_type=str):
-    return prefix + row.fillna(empty_value).astype(as_type).values[index]
+    if as_type == int:
+        if row.fillna(empty_value).values[index]:
+            return f"{prefix}{str(int(row.fillna(empty_value).values[index]))}"
+        else:
+            return ""
+    else:
+        return  f"{prefix}{row.fillna(empty_value).astype(as_type).values[index]}"
+
+
+def format_to_decimal(value):
+    return '{:.2f}'.format(float(value))
 
 
 class CONST:
@@ -23,6 +33,7 @@ class CONST:
     CMD = "CMD"
     REQUESTER = "Solicitante"
     CATEGORY = "Categoria"
+    SUBCATEGORY = "Sub-categoria\n(selecionar da lista abaixo)"
     STARTED_WITH_1000 = "Imobilizado\n(começado c/ 1000)"
     STARTED_WITH_8000 = "Disponível para venda\n(começado c/ 8000)"
     QTD = "Qte"
@@ -61,21 +72,38 @@ class ValeMEConverter(SpreadsheetConverter):
 
         sheet_row_02 = pandas.read_excel(file, header=9, usecols="C:O")
 
-        lot_name = ma_utils.generate_description_from_array(
-            sheet_row_02[CONST.DESCRIPTION].dropna().values
-        )
-
         location = ma_utils.split_city_and_state(
             handle_sheet_row(sheet_row_01[CONST.LOCATION])
         )
-        total_amount = int(sheet_row_02[CONST.TOTAL_AMOUNT].dropna().tail(1))
-        vi = round(float(total_amount), 2) * 0.3
+        total_amount = float(sheet_row_02[CONST.TOTAL_AMOUNT].dropna().tail(1))
+        vi = float(total_amount) * 0.3
 
         total_items = sheet_row_02["Qte"].dropna()
 
-        description = ""
+        description = handle_sheet_row(sheet_row_02[CONST.STARTED_WITH_1000], 0, "Código do Produto: ", "", int)
+        description += handle_sheet_row(sheet_row_02[CONST.STARTED_WITH_8000], 0, "<br>Referência do Produto: ", "", int)
+        description += handle_sheet_row(sheet_row_02[CONST.DESCRIPTION], 0, "<br>Descrição do Produto: ", "")
+        description += handle_sheet_row(sheet_row_02[CONST.SUBCATEGORY], 0, "<br>Descrição do Grupo: ", "")
+        description += handle_sheet_row(sheet_row_02[CONST.QTD], 0, "<br>Quantidade: ", "")
+        description += handle_sheet_row(sheet_row_02[CONST.UN], 0, "<br>Unidade: ", "")
+        description += handle_sheet_row(sheet_row_02[CONST.WEIGHT], 0, "<br>Peso Estimado: ", "")
+        description += handle_sheet_row(sheet_row_01[CONST.LOT], 0, "<br>Lote de Referência: ", "")
+
+        html_description = ""
+        lot_name = handle_sheet_row(sheet_row_02[CONST.DESCRIPTION])
         if len(total_items) > 1:
+            lot_name = ma_utils.generate_description_from_array(
+                sheet_row_02[CONST.DESCRIPTION].dropna().values
+            )
             description = "Para maiores informações, clique em ANEXOS"
+            html_description = "Em arquivo separado"
+
+        subcategories = list(set(sheet_row_02[CONST.SUBCATEGORY].dropna().values))
+
+        subcategory = ""
+
+        if len(subcategories) == 1:
+            subcategory = subcategories[0]
 
         df = pandas.DataFrame(
             {
@@ -103,8 +131,9 @@ class ValeMEConverter(SpreadsheetConverter):
                 "Unid. Métrica": "",
                 "Fator Multiplicativo": 1,
                 "Alteração/Adicionado": "",
-                "Descrição HTML": "",
+                "Descrição HTML": html_description,
                 "Categoria": handle_sheet_row(sheet_row_01[CONST.CATEGORY]),
+                "Subcategoria": subcategory,
             },
             index=[0],
         )
@@ -123,54 +152,52 @@ class ValeMEConverter(SpreadsheetConverter):
             "Lote de Referência",
         ]
 
-        if len(total_items) > 1:
-            for index, item in enumerate(total_items):
-                append_data = {
-                    "Código do Produto": handle_sheet_row(
-                        sheet_row_02[CONST.STARTED_WITH_1000], index
-                    ),
-                    "Referência do Produto": handle_sheet_row(
-                        sheet_row_02[CONST.STARTED_WITH_8000], index
-                    ),
-                    "Descrição do Produto": handle_sheet_row(
-                        sheet_row_02[CONST.DESCRIPTION], index
-                    ),
-                    "Descrição do Grupo": lot_name,
-                    "Quantidade": handle_sheet_row(sheet_row_02[CONST.QTD], index),
-                    "Unidade": handle_sheet_row(sheet_row_02[CONST.UN], index),
-                    "Valor Unitário": handle_sheet_row(
-                        sheet_row_02[CONST.UNIT_PRICE], index
-                    ),
-                    "Valor Total": handle_sheet_row(
-                        sheet_row_02[CONST.TOTAL_AMOUNT], index
-                    ),
-                    "Peso Estimado": handle_sheet_row(
-                        sheet_row_02[CONST.WEIGHT], index
-                    ),
-                    "Lote de Referência": handle_sheet_row(sheet_row_01[CONST.LOT]),
-                }
-                temp_list.append(append_data)
+        for index, item in enumerate(total_items):
+            append_data = {
+                "Código do Produto": handle_sheet_row(
+                    sheet_row_02[CONST.STARTED_WITH_1000], index, "", "", int
+                ),
+                "Referência do Produto": handle_sheet_row(
+                    sheet_row_02[CONST.STARTED_WITH_8000], index, "", "", int
+                ),
+                "Descrição do Produto": handle_sheet_row(
+                    sheet_row_02[CONST.DESCRIPTION], index
+                ),
+                "Descrição do Grupo": handle_sheet_row(
+                    sheet_row_02[CONST.SUBCATEGORY], index
+                ),
+                "Quantidade": format_to_decimal(handle_sheet_row(sheet_row_02[CONST.QTD], index)),
+                "Unidade": handle_sheet_row(sheet_row_02[CONST.UN], index),
+                "Valor Unitário": format_to_decimal(handle_sheet_row(sheet_row_02[CONST.UNIT_PRICE], index)),
+                "Valor Total": format_to_decimal(handle_sheet_row(sheet_row_02[CONST.TOTAL_AMOUNT], index)),
+                "Peso Estimado": format_to_decimal(handle_sheet_row(
+                    sheet_row_02[CONST.WEIGHT], index
+                )),
+                "Lote de Referência": handle_sheet_row(sheet_row_01[CONST.LOT]),
+            }
+            temp_list.append(append_data)
 
-            logger.info("Carregando um dataset para geração do arquivo HTML")
-            df_html = pandas.DataFrame(temp_list, columns=df_columns)
+            if len(total_items) > 1:
+                logger.info("Carregando um dataset para geração do arquivo HTML")
+                df_html = pandas.DataFrame(temp_list, columns=df_columns)
 
-            logger.info("Gerando código HTML a partir dos dados do dataset")
-            html_content = df_html.to_html(index=False, na_rep="")
+                logger.info("Gerando código HTML a partir dos dados do dataset")
+                html_content = df_html.to_html(index=False, na_rep="")
 
-            logger.info("Concatenando o cabeçalho e o rodapé do arquivo HTML")
-            html_content = (
-                html_utils.get_header() + html_content + html_utils.get_footer()
-            )
+                logger.info("Concatenando o cabeçalho e o rodapé do arquivo HTML")
+                html_content = (
+                        html_utils.get_header() + html_content + html_utils.get_footer()
+                )
 
-            html_file = open(
-                os.path.join("output", "html", file_name + ".html"),
-                "w",
-                newline="",
-                encoding="utf-8",
-            )
+                html_file = open(
+                    os.path.join("output", "html", os_utils.get_file_name(file_name) + ".html"),
+                    "w",
+                    newline="",
+                    encoding="utf-8",
+                )
 
-            html_file.write(html_content)
-            html_file.close()
+                html_file.write(html_content)
+                html_file.close()
 
         df_list = pandas.DataFrame(temp_list, columns=df_columns)
 
@@ -190,8 +217,11 @@ class ValeMEConverter(SpreadsheetConverter):
             df_list = pandas.concat([append_list, df_list])
 
         writer = pandas.ExcelWriter(self._output_xlsx_file, engine="xlsxwriter")
-        df.to_excel(writer, sheet_name="Colunada", index=False)
-        df_list.to_excel(writer, sheet_name="Listagem", index=False)
+
+        df.sort_values("Nº do lote", ascending=True).to_excel(writer, sheet_name="Colunada", index=False)
+        df_list.sort_values(["Lote de Referência", "Código do Produto"], ascending=True).to_excel(writer,
+                                                                                                  sheet_name="Listagem",
+                                                                                                  index=False)
 
         writer.save()
 
