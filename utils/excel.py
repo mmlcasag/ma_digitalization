2879d9af3cd4d7a15c6237a20572ff56e393fa86
Francis Gregori <francis.g.munis@gmail.com>
2021-07-08 12:06:04 -0300

    refactor: add emf conditional to extract_images_from_xlsx

--- utils/excel.py
+++ utils/excel.py
@@ -103,8 +103,14 @@ def extract_images_from_xlsx(file, output_folder):
         os_utils.create_folder(output_folder)
         for idx, file in enumerate(files_from_xlsx):
             origin_file = os.path.join(origin_folder, file)
-            destination_file = os.path.join(output_folder, f"imagem_{idx+1}.jpg")
-            shutil.copy(origin_file, destination_file)
-            resize_image(destination_file)
+            destination_file = os.path.join(output_folder, f"imagem_{idx+1}")
+
+            if os.name == "nt":
+                Image.open(origin_file).save(f"{destination_file}.jpg")
+            else:
+                ext = 'jpg'
+                if os_utils.get_file_extension(origin_file) == 'emf':
+                    ext = 'emf'
+                shutil.copy(origin_file, f"{destination_file}.{ext}")
 
         shutil.rmtree(tempdir)
